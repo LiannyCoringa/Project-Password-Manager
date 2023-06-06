ef033ba3a404191f060bf80e65b3de70ca222b59
Lianny Coringa <lianny_costa@hotmail.com>
2023-06-06 15:54:54 -0300

    metade do cinco

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,10 +8,32 @@ function Form() {
     Senha: false,
   });
   const { NomeDoServiço, Login, Senha } = validation;
+  const classValidation = 'valid-password-check';
+  const classInvalidation = 'invalid-password-check';
+  const [validationPassword, setValidationPassword] = useState({
+    minLength: classInvalidation,
+    maxLength: classValidation,
+    numbersAndT: classInvalidation,
+    caracRegex: classInvalidation,
+  });
+  const { minLength, maxLength, numbersAndT, caracRegex } = validationPassword;
   const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
-  const regexFunction = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const regexs = regex.test(event.target.value)
+  const regexNumbers = /^(?=.*\d)[0-9]/;
+  const regexText = /^(?=.*[a-zA-Z])/;
+  const regexCaracEspecial = /^(?=.*[$*&@#])/;
+  const regexFunction = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const regexV = regex.test(target.value)
      && setValidation({ ...validation, Senha: true });
+    const minLengthV = target.value.length < 8
+      ? setValidationPassword({ ...validationPassword, minLength: classInvalidation })
+      : setValidationPassword({ ...validationPassword, minLength: classValidation });
+    const maxLengthV = target.value.length >= 16
+      ? setValidationPassword({ ...validationPassword, maxLength: classInvalidation })
+      : setValidationPassword({ ...validationPassword, maxLength: classValidation });
+    const regexTandNV = regexNumbers.test(target.value) && regexText.test(target.value)
+      && setValidationPassword({ ...validationPassword, numbersAndT: classValidation });
+    const regexCaracV = regexCaracEspecial.test(target.value)
+      && setValidationPassword({ ...validationPassword, caracRegex: classValidation });
   };
   return (
     <div>
@@ -39,6 +61,10 @@ function Form() {
               maxLength={ 16 }
               onChange={ regexFunction }
             />
+            <p className={ minLength }>Possuir 8 ou mais caracteres</p>
+            <p className={ maxLength }>Possuir até 16 caracteres</p>
+            <p className={ numbersAndT }>Possuir letras e números</p>
+            <p className={ caracRegex }>Possuir algum caractere especial</p>
             <label htmlFor="URL">URL</label>
             <input id="URL" type="text" />
             {NomeDoServiço && Login && Senha
